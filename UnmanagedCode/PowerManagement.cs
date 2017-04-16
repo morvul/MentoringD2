4d26db800973329a1b098bef65ace1b6ef054508
Morvul <morvull@gmail.com>
2017-04-17 01:31:17 +0300

    Task1.1

---
+++ UnmanagedCode/PowerManagement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace UnmanagedCode
+{
+    public static class PowerManagement
+    {
+        private enum InfoLevel
+        {
+            SystemPowerInformation = 14
+        }
+
+
+        #region WinApi functions
+
+        [DllImport("kernel32")]
+        private static extern ulong GetTickCount64();
+
+        [DllImport("powrprof.dll")]
+        public static extern uint CallNtPowerInformation(
+            int informationLevel,
+            IntPtr inputBuffer,
+            uint inputBufferSize,
+            out ulong outputBuffer,
+            uint outputBufferSize);
+
+        #endregion
+
+
+        public static DateTime GetLastWakeTime()
+        {
+            var outputBufferSize = Marshal.SizeOf(typeof(ulong));
+            ulong ticksElapsed;
+            var result = CallNtPowerInformation(
+                (int)InfoLevel.SystemPowerInformation,
+                (IntPtr) null,
+                0,
+                out ticksElapsed,
+                (uint)outputBufferSize);
+
+            var lastWakeTime = new DateTime((uint)ticksElapsed);
+            return lastWakeTime;
+        }
+    }
+}
---
+++ UnmanagedCode/PowerManagementTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnmanagedCode
+{
+    [TestClass]
+    public class PowerManagementTests
+    {
+        [TestMethod]
+        public void LastWakeTime_Task1()
+        {
+            Console.WriteLine(PowerManagement.GetLastWakeTime());
+        }
+    }
+}
