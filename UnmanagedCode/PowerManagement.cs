0411d6564f59632bafa8818600b15cf23d651d64
Morvul <morvull@gmail.com>
2017-04-27 00:11:31 +0300

    Interoperations Task1

--- UnmanagedCode/PowerManagement.cs
+++ UnmanagedCode/PowerManagement.cs
@@ -5,10 +5,45 @@ namespace UnmanagedCode
 {
     public static class PowerManagement
     {
-        private enum InfoLevel
+        #region Structs
+
+        public enum InfoLevel
         {
-            SystemPowerInformation = 14
+            SystemBatteryState = 5,
+            SystemHiberFileControl = 10,
+            SystemPowerInformation = 12,
+            LastWakeTime = 14,
+            LastSleepTime = 15
         }
+
+        public enum HiberFileOperation
+        {
+            RemoveHiberFile = 0,
+            ReserveHiberFile = 1,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SystemBatteryState
+        {
+            [MarshalAs(UnmanagedType.I1)]
+            public bool AcOnLine;
+            [MarshalAs(UnmanagedType.I1)]
+            public bool BatteryPresent;
+            [MarshalAs(UnmanagedType.I1)]
+            public bool Charging;
+            [MarshalAs(UnmanagedType.I1)]
+            public bool Discharging;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4, ArraySubType = UnmanagedType.I1)]
+            public bool[] Spare1;
+            public int MaxCapacity;
+            public int RemainingCapacity;
+            public int Rate;
+            public int EstimatedTime;
+            public int DefaultAlert1;
+            public int DefaultAlert2;
+        }
+
+        #endregion
 
 
         #region WinApi functions
@@ -16,30 +51,120 @@ namespace UnmanagedCode
         [DllImport("kernel32")]
         private static extern ulong GetTickCount64();
 
+
+        [DllImport("powrprof.dll")]
+        public static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+
         [DllImport("powrprof.dll")]
         public static extern uint CallNtPowerInformation(
-            int informationLevel,
+            InfoLevel informationLevel,
             IntPtr inputBuffer,
-            uint inputBufferSize,
-            out ulong outputBuffer,
-            uint outputBufferSize);
+            int inputBufferSize,
+            [MarshalAs(UnmanagedType.U8)]
+            out long result,
+            int resultSize);
+
+        [DllImport("powrprof.dll")]
+        public static extern uint CallNtPowerInformation(
+            InfoLevel informationLevel,
+            IntPtr inputBuffer,
+            int inputBufferSize,
+            [MarshalAs(UnmanagedType.Struct)]
+            out SystemBatteryState result,
+            int resultSize);
+
+
+        [DllImport("powrprof.dll")]
+        public static extern uint CallNtPowerInformation(
+            InfoLevel informationLevel,
+            IntPtr inputParam,
+            int inputParamSize,
+            IntPtr result,
+            int resultSize);
 
         #endregion
 
 
+        #region Power information getting
+
         public static DateTime GetLastWakeTime()
         {
-            var outputBufferSize = Marshal.SizeOf(typeof(ulong));
-            ulong ticksElapsed;
-            var result = CallNtPowerInformation(
-                (int)InfoLevel.SystemPowerInformation,
-                (IntPtr) null,
+            return GetTime(InfoLevel.LastWakeTime);
+        }
+
+        public static DateTime GetLastSleepTime()
+        {
+            return GetTime(InfoLevel.LastSleepTime);
+        }
+
+        public static SystemBatteryState GetSystemBatteryState()
+        {
+            var resultSize = Marshal.SizeOf<SystemBatteryState>();
+            SystemBatteryState result;
+            CallNtPowerInformation(
+                InfoLevel.SystemBatteryState,
+                IntPtr.Zero,
+                0,
+                out result,
+                resultSize);
+            return result;
+        }
+
+        private static DateTime GetTime(InfoLevel infoLevel)
+        {
+            var resultSize = sizeof (ulong);
+            long ticksElapsed;
+            CallNtPowerInformation(
+                infoLevel,
+                IntPtr.Zero,
                 0,
                 out ticksElapsed,
-                (uint)outputBufferSize);
+                resultSize);
 
-            var lastWakeTime = new DateTime((uint)ticksElapsed);
-            return lastWakeTime;
+            var startupTicksElapsed = GetTickCount64()*10000;
+            var result = DateTime.Now - TimeSpan.FromTicks((long) startupTicksElapsed) +
+                         TimeSpan.FromTicks(ticksElapsed);
+            return result;
+        }
+
+        #endregion
+
+        #region Hibernation file operations
+
+        public static bool ReserveHiberFile()
+        {
+            return HiberFileControl(HiberFileOperation.ReserveHiberFile);
+        }
+
+        public static bool RemoveHiberFile()
+        {
+            return HiberFileControl(HiberFileOperation.RemoveHiberFile);
+        }
+
+        public static bool HiberFileControl(HiberFileOperation operation)
+        {
+            var inputBufferSize = sizeof (HiberFileOperation);
+            var inputBuffer = Marshal.AllocHGlobal(inputBufferSize);
+
+            Marshal.WriteInt32(inputBuffer, 0, (int)operation);
+            var errorCode = CallNtPowerInformation(InfoLevel.SystemHiberFileControl, inputBuffer, inputBufferSize, IntPtr.Zero, 0);
+
+            Marshal.FreeHGlobal(inputBuffer);
+            return errorCode == 0;
+        }
+
+        #endregion
+
+        public static bool Sleep()
+        {
+            var result = SetSuspendState(false, false, false);
+            return result;
+        }
+
+        public static bool Hibernate()
+        {
+            var result = SetSuspendState(true, false, false);
+            return result;
         }
     }
 }
--- UnmanagedCode/PowerManagementTests.cs
+++ UnmanagedCode/PowerManagementTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnmanagedCode
@@ -9,7 +11,68 @@ namespace UnmanagedCode
         [TestMethod]
         public void LastWakeTime_Task1()
         {
-            Console.WriteLine(PowerManagement.GetLastWakeTime());
+            Console.WriteLine($"Last wake time: {PowerManagement.GetLastWakeTime()}");
+        }
+
+        [TestMethod]
+        public void LastSleepTime_Task1()
+        {
+            Console.WriteLine($"Last sleep time: {PowerManagement.GetLastSleepTime()}");
+        }
+
+        [TestMethod]
+        public void GetSystemBatteryState_Task1()
+        {
+            var battaryState = PowerManagement.GetSystemBatteryState();
+            var fields = typeof(PowerManagement.SystemBatteryState).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine("System Battery State:\n");
+            foreach (var field in fields)
+            {
+                var value = field.GetValue(battaryState);
+                if (field.FieldType.IsArray)
+                {
+                    var arrayValue = new StringBuilder();
+                    foreach (var info in (Array)value)
+                    {
+                        if (arrayValue.Length > 0)
+                        {
+                            arrayValue.Append(", ");
+                        }
+
+                        arrayValue.Append(info);
+                    }
+
+                    Console.WriteLine($"{field.Name}: {arrayValue}");
+                }
+                else
+                {
+                    Console.WriteLine($"{field.Name}: {value}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void RemoveHiberFile_Task1()
+        {
+            Console.WriteLine($"Hibernation file was{(PowerManagement.RemoveHiberFile() ? " succesfully" : "n't")} removed");
+        }
+
+        [TestMethod]
+        public void ReserveHiberFile_Task1()
+        {
+            Console.WriteLine($"Hibernation file was{(PowerManagement.ReserveHiberFile() ? " succesfully" : "n't")} reserved");
+        }
+
+        [TestMethod]
+        public void Sleep_Task1()
+        {
+            Console.WriteLine($"{(PowerManagement.Sleep() ? "Bye bye..." : "Can't sleep")}");
+        }
+
+        [TestMethod]
+        public void Hibernate_Task1()
+        {
+            Console.WriteLine($"{(PowerManagement.Hibernate() ? "Good night..." : "Can't sleep")}");
         }
     }
 }
