4c51e69e5ad485d47719146e91373d8e9bd751ee
GOMEL\Uladzimir_Mardvinau <Uladzimir_Mardvinau@epam.com>
2017-05-29 20:24:05 +0300

    Cebugging task implementation

---
+++ 7.Debugging/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows;
+
+namespace Debugging
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void GenerateCommand(object sender, RoutedEventArgs e)
+        {
+            var networkInterface = NetworkInterface.GetAllNetworkInterfaces().First();
+            var networkBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+            var dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            var resultBytes = networkBytes.Select((a, b) => a ^ dateBytes[b]);
+            Password.Text = string.Join("-", resultBytes.Select(b => b >= 999 ? b : b * 10));
+        }
+    }
+}
